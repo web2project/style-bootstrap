0a77d4c143aa4d2a675479bd9d3287b9066838b2
D Keith Casey Jr <keith@caseysoftware.com>
2013-05-09 02:56:25 -0500

    corrected the paths

--- footer.php
+++ footer.php
@@ -10,16 +10,16 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./style/<?php echo $uistyle; ?>/js/jquery.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-transition.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-alert.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-modal.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-dropdown.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-scrollspy.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-tab.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-tooltip.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-popover.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-button.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-collapse.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-carousel.js"></script>
-    <script src="./style/<?php echo $uistyle; ?>/js/bootstrap-typeahead.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/jquery.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-transition.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-alert.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-modal.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-dropdown.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-tab.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-tooltip.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-popover.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-button.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-collapse.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-carousel.js"></script>
+    <script src="./style/<?php echo $uistyle; ?>/assets/js/bootstrap-typeahead.js"></script>
