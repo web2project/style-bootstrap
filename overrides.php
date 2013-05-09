de50b417fed251f643e0520ad239e2aff5a5cf9b
D Keith Casey Jr <keith@caseysoftware.com>
2013-05-09 03:18:19 -0500

    wiring some more style stuff

--- overrides.php
+++ overrides.php
@@ -22,11 +22,7 @@ class CTabBox extends w2p_Theme_TabBox {
             echo '<table border="0" cellpadding="2" cellspacing="0" width="100%"><tr>' . $extra . '</tr>' . '</table>';
         }
 
-        // tabbed view
-//echo '<pre>'; print_r($this->tabs); echo '</pre>';
 //TODO: check count before drawing anything
-
-
         $s .= '<table width="100%" border="0" cellpadding="0" cellspacing="0">';
 
         $s .= '<tr><td>';
